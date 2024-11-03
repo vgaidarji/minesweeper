7ec9e9cd55694b3ec6d96783298587c2d68867ec
vgaidarji <veaceslav.gaidarji@gmail.com>
2024-11-03 15:15:28 +0200

    Initialize empty .NET MAUI project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Minesweeper
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Minesweeper
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
