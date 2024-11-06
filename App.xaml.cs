36e767ee8a76dbb5ebb6796a1e728fb5aca95d6f
vgaidarji <veaceslav.gaidarji@gmail.com>
2024-11-06 22:04:50 +0200

    Set app window width/height in App.xaml

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,11 +2,30 @@
 {
     public partial class App : Application
     {
+        private const int APP_WINDOW_WIDTH = 331;
+        private const int APP_WINDOW_HEIGHT = 419;
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            // https://putridparrot.com/blog/maui-setting-the-apps-dimensions-on-a-windows-desktop/
+            #if WINDOWS
+            if (DeviceInfo.Idiom == DeviceIdiom.Desktop)
+            {
+                window.Width = APP_WINDOW_WIDTH;
+                window.Height = APP_WINDOW_HEIGHT;
+            }
+            #endif
+
+            return window;
         }
     }
 }
